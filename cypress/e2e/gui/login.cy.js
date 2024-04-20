bad39ddd40c96754522126e39174b1e8f0b575af
¡Duque <atanaelleonardo@gmail.com>
2024-04-19 22:52:35 -0300

    "Add new test for logging out and update existing login test in GUI spec"
    This commit includes a change to the `cypress/e2e/gui/login.cy.js` file, where a new test for logging

--- cypress/e2e/gui/login.cy.js
+++ cypress/e2e/gui/login.cy.js
@@ -1,12 +1,12 @@
 describe('Login', () => {
-  it('successfully', (user = Cypress.env('user_name'), password = Cypress.env(
-    'user_password',
-  )) => {
-    cy.visit('/users/sign_in')
-    cy.get("[data-qa-selector='login_field']").type(user)
-    cy.get("[data-qa-selector='password_field']").type(password, { log: false })
-    cy.get("[data-qa-selector='sign_in_button']").click()
+  it('successfully', () => {
+    cy.login()
 
     cy.get('.qa-user-avatar').should('be.visible')
+  })
+
+  it.only('should logout', () => {
+    cy.get('.qa-user-avatar').should('be.visible').click()
+    cy.get('li.sign-out-link').should('be.visible').click()
   })
 })
