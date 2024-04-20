00290f54a0c0faebf142b21468036c0ea5b8be3a
¡Duque <atanaelleonardo@gmail.com>
2024-04-19 22:53:30 -0300

    Remove logout test from login spec and add new logout spec
    The login spec no longer contains a test for logout. A new logout spec has been added with a single test case for successful logout.

--- cypress/e2e/gui/login.cy.js
+++ cypress/e2e/gui/login.cy.js
@@ -4,9 +4,4 @@ describe('Login', () => {
 
     cy.get('.qa-user-avatar').should('be.visible')
   })
-
-  it.only('should logout', () => {
-    cy.get('.qa-user-avatar').should('be.visible').click()
-    cy.get('li.sign-out-link').should('be.visible').click()
-  })
 })
