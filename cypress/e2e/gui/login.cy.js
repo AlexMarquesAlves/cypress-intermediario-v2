2e678b9c20c3794934ea96b7880851b12715d909
¡Duque <atanaelleonardo@gmail.com>
2024-04-19 22:33:29 -0300

    "Change GUI login test to use Cypress environment variables and check for user avatar after login"
    This commit updates the GUI login test in Cypress by using environment variables for the user name and password. After logging in, it

--- cypress/e2e/gui/login.cy.js
+++ cypress/e2e/gui/login.cy.js
@@ -1,5 +1,12 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Login', () => {
+  it('successfully', (user = Cypress.env('user_name'), password = Cypress.env(
+    'user_password',
+  )) => {
+    cy.visit('/users/sign_in')
+    cy.get("[data-qa-selector='login_field']").type(user)
+    cy.get("[data-qa-selector='password_field']").type(password, { log: false })
+    cy.get("[data-qa-selector='sign_in_button']").click()
+
+    cy.get('.qa-user-avatar').should('be.visible')
   })
 })
